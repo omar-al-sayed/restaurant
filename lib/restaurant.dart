3225310c1698c01579197c745c1184fce78f3d76
omar-al-sayed <omar.al.sayed.99.oas@gmail.com>
2023-11-23 13:06:32 +0200

    add assets folder and 3 images add restaurant file and dropdownMenu and ListView and list of items

---
+++ lib/restaurant.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class Restaurant extends StatefulWidget {
+  const Restaurant({Key? key}) : super(key: key);
+
+  @override
+  State<Restaurant> createState() => _RestaurantState();
+}
+
+class _RestaurantState extends State<Restaurant> {
+  final List<Map<String, dynamic>> menuItems = [
+    {'name': 'Burger', 'price': 8.99, 'image': 'assets/burger.jpg'},
+    {'name': 'Pizza', 'price': 12.99, 'image': 'assets/pizza.jpg'},
+    {'name': 'Salad', 'price': 6.99, 'image': 'assets/salad.jpg'},
+  ];
+
+
+  String selectedItem = 'Burger';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Restaurant'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton<String>(
+              value: selectedItem,
+              items: menuItems.map((item) {
+                return DropdownMenuItem<String>(
+                  value: item['name'],
+                  child: Text(item['name']),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedItem = value!;
+                });
+              },
+            ),
+
+            SizedBox(height: 20),
+            Expanded(
+              //list view
+              child: ListView.builder(
+                itemCount: menuItems.length,
+                itemBuilder: (context, index) {
+                  final item = menuItems[index];
+                  return Card(
+                    elevation: 3,
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    child: ListTile(
+                      title: Text(item['name']),
+                      subtitle: Text('\$${item['price']}'),
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage(item['image']),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          selectedItem = item['name'];
+                        });
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
